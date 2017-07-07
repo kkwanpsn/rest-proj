03be579fc54655c35902a13042cc0dd1aaa04594
Kwan <kkwan@us784f4377d153.am.sony.com>
2017-07-06 17:25:54 -0700

    Enunciate integration

--- src/main/java/com/rest/test/HelloWorld.java
+++ src/main/java/com/rest/test/HelloWorld.java
@@ -9,6 +9,10 @@ import javax.ws.rs.DefaultValue;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
+import com.webcohesion.enunciate.metadata.rs.StatusCodes;
+import com.webcohesion.enunciate.metadata.rs.ResponseCode;
+import com.webcohesion.enunciate.metadata.rs.TypeHint;
+
 /**
  * This is a sample REST API service for the purpose of demonstrating
  * Enunciate/Swagger integration.
@@ -22,9 +26,14 @@ public class HelloWorld {
      * Returns a customized greeting.
      * @param name Name of the person to greet.
      * @return The greeting.
+     * @HTTP 500 Internal server error.
+     * @HTTP 404 Greeting not found.
      */
     @GET
     @Produces(MediaType.TEXT_PLAIN)
+    @StatusCodes({
+            @ResponseCode(code=500, condition="Internal Kamaji server error.", type=@TypeHint(Response.class))
+                })
     public Response HelloWorld(@DefaultValue("World") @QueryParam("name") String msg) {
 
         String result = "Hello " + msg + "!";
